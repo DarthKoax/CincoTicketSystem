bdc1e9ca07127589c937301a8e713961323b594f
darthkoax <s3655767@student.rmit.edu.au>
2019-10-28 12:19:22 +1100

    add powershell script

---
+++ src/powershell_compile_run.ps1
@@ -0,0 +1,7 @@
+cd com\example\java\
+javac Main.java SerializeList.java TechMenu.java Tests.java Ticket.java TicketList.java UserMenu.java
+cd ..\..\..\
+java com.example.java.Main
+cd com\example\java\
+rm *.Class
+cd ..\..\..\
