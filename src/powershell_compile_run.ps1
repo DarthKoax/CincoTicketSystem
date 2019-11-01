fd6a501cd462a8ba0fcf2426468b311ced22d21c
DarthKoax <darthkoax@KylePC.localdomain>
2019-11-01 14:28:28 +1100

    ammend scripts

--- src/powershell_compile_run.ps1
+++ src/powershell_compile_run.ps1
@@ -1,5 +1,5 @@
 cd com\example\java\
-javac Main.java SerializeList.java TechMenu.java Tests.java Ticket.java TicketList.java UserMenu.java
+javac Main.java SerializeList.java TechMenu.java Tests.java Ticket.java TicketList.java UserMenu.java Technician.java TechnicianList.java
 cd ..\..\..\
 java com.example.java.Main
 cd com\example\java\
