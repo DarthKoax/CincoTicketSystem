299914e8b19c3bda3020ff6e5ee4f7815c37a02d
Jay <s3404374@student.rmit.edu.au>
2019-11-03 14:19:43 +1100

    Start changing technicla level menu

--- src/com/example/java/TechMenu.java
+++ src/com/example/java/TechMenu.java
@@ -12,6 +12,7 @@ public class TechMenu {
     SerializeList cereal = new SerializeList();
     // ArrayList<Ticket> tickets = new ArrayList<Ticket>();
     TicketList tickets = cereal.readTicketFile();
+    int numOpenTickets;
 
     public static final String LINE1 = "-------------------------------------------------";
     public static final String LINE2 = "|          CINCO  TICKET ISSUING SYSTEM         |";
@@ -24,17 +25,23 @@ public class TechMenu {
     public static final String LINE9 = "               LEVEL 1 SERVICE DESK TICKETS    ";
     public static final String LINE10 = "               LEVEL 2 SERVICE DESK TICKETS    ";
 
-    
+
     public String loginOption;
 
-    Stream<String> techMenuStream = Stream
-            .of(new String[] { LINE1, LINE2, LINE3, LINE4, LINE5, LINE6, LINE8, LINE4, LINE1 });
 
-    Stream<String> techLevel1Stream = Stream.of(new String[] { LINE1, LINE7, LINE9, LINE7, LINE7 });
+    Stream<String> techLevel1Stream = Stream.of(LINE1, LINE7, LINE9, LINE7, LINE7);
 
-    Stream<String> techLevel2Stream = Stream.of(new String[] { LINE1, LINE7, LINE10, LINE7, LINE7 });
+    Stream<String> techLevel2Stream = Stream.of(LINE1, LINE7, LINE10, LINE7, LINE7);
 
     public void printTechMenu() {
+        // the number of spaces in line 11 after displaying 1 digit is
+        int spacesLine11 = 12;
+        numOpenTickets = tickets.getNumberOpenTickets();
+        String spaceL11 = getSpaceLeft(spacesLine11, numOpenTickets);
+        String LINE11 = "|           NUMBER OF OPEN TICKETS: " + numOpenTickets + spaceL11 + "|";
+
+        Stream<String> techMenuStream = Stream
+                .of(LINE1, LINE2, LINE3, LINE4, LINE11, LINE4, LINE5, LINE6, LINE8, LINE4, LINE1);
         techMenuStream.forEach(p -> System.out.println(p));
         System.out.println("Enter number for Service desk or Option 3 to Exit");
         loginOption = getServiceDesk();
@@ -53,22 +60,95 @@ public class TechMenu {
 
     public void validateLogin(String loginOption) {
         switch (loginOption) {
-        case "1":
-            techLevel1Stream.forEach(p -> System.out.println(p));
-            tickets.getTicketsLowAndMedium();
-            break;
-        case "2":
-            techLevel2Stream.forEach(p -> System.out.println(p));
-            tickets.getTicketsHigh();
-            break;
-        case "3":
-            System.out.println("Exit the system ");
-            // System.exit(0);
-            return;
-        default:
-            System.out.println("Something went very wrong");
+            case "1":
+                level1Login();
+                break;
+            case "2":
+                level2Login();
+                break;
+            case "3":
+                System.out.println("Exit the system ");
+                return;
+            default:
+                System.out.println("Something went very wrong");
         }
     }
-    
+
+    public String getSpaceLeft(Integer sl, Integer num) {
+        int spaceLength = sl;
+        int numL = String.valueOf(num).length();
+        int count = sl - numL;
+        String spaceCalc = String.format("%" + count + "c", ' ');
+        return spaceCalc;
+    }
+
+    public void level1Login() {
+        techLevel1Stream.forEach(p -> System.out.println(p));
+        tickets.getTicketsListLowAndMedium();
+        int option = getLoginOption();
+        if (option > 0) {
+            tickets.getRequestedTicket(option);
+            changeSeverity();
+            changeStatus();
+        }
+    }
+
+    public void level2Login() {
+        techLevel2Stream.forEach(p -> System.out.println(p));
+        tickets.getTicketsListHigh();
+        int option = getLoginOption();
+        if (option > 0) {
+            tickets.getRequestedTicket(option);
+            changeSeverity();
+            changeStatus();
+        }
+    }
+
+    public int getLoginOption() {
+        int option = 0;
+        boolean isNumeric = false;
+        do {
+            System.out.println("Enter number of ticket to view or 0 to exit:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("That is not a valid number, Please enter again");
+                scanner.next();
+            }
+            isNumeric = true;
+            option = scanner.nextInt();
+            scanner.nextLine();
+        } while (isNumeric == false);
+        return option;
+    }
+
+    public void changeSeverity() {
+        System.out.println("Do you want to change the severity of the ticket?");
+        String option = getSubmit();
+        if (option.equalsIgnoreCase("Y")) {
+            System.out.println("Change the severity here");
+        }
+    }
+
+    public void changeStatus() {
+        System.out.println("Do you want to change the status of the ticket?");
+        String option = getSubmit();
+        if (option.equalsIgnoreCase("Y")) {
+            System.out.println("Change the status here");
+        }
+    }
+
+    /**
+     * Submit can only be Y or N
+     *
+     * @return
+     */
+    public String getSubmit() {
+
+        String submitAnswer = null;
+        do {
+            System.out.println("Enter Y for YES or N for NO");
+            submitAnswer = scanner.nextLine();
+        } while (!submitAnswer.equalsIgnoreCase("Y") && !submitAnswer.equalsIgnoreCase("N"));
+        return submitAnswer;
+    }
 
 }
