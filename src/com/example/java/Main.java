9938f752162dbbce79313f24352db6e4de7d7fcd
DarthKoax <darthkoax@KylePC.localdomain>
2019-11-12 14:37:35 +1100

    add note for seedtickets()

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -108,12 +108,13 @@ public class Main {
 
         if (tickets.size() == 0) {
 
-            
+            //NOTE: Month field starts at 0
+            //IE November = 10 OR 11-1
             Ticket ticket1 = new Ticket("Bob", "Brown", "5555", "bob.brown@cinco.com", "044555787", "No network connection", "HIGH","open", 2019,11-1,12);
             tickets.add(ticket1);
             cereal.writeTicketFile(tickets);  
-            
-            Ticket ticket2 = new Ticket("Frank", "Buster", "6578", "Frank.Buster@cinco.com", "04457558", "Computer wont turn on", "LOW","open", 2019,9,12);
+
+            Ticket ticket2 = new Ticket("Frank", "Buster", "6578", "Frank.Buster@cinco.com", "04457558", "Computer wont turn on", "LOW","open", 2019,9-1,12);
             tickets.add(ticket2);
             cereal.writeTicketFile(tickets);
         }
