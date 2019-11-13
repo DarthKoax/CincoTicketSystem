8c75ed57b0ee2c7926e897cb28dcc6b2af329054
Jay <s3404374@student.rmit.edu.au>
2019-11-13 21:56:21 +1100

    remove ser

---
+++ out/production/CincoTicketSystem/bin/withTestRun.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -ea com/example/java/Main
---
+++ out/production/CincoTicketSystem/powershell_compile_run.ps1
@@ -0,0 +1,7 @@
+cd com\example\java\
+javac Main.java SerializeList.java TechMenu.java Tests.java Ticket.java TicketList.java UserMenu.java Technician.java TechnicianList.java
+cd ..\..\..\
+java com.example.java.Main
+cd com\example\java\
+rm *.Class
+cd ..\..\..\
